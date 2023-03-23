ee33abb96567099930d637f6e8a80d6fa920d579
Faiz Alkautsar <alkafaiz99@gmail.com>
2023-03-23 15:47:30 +0800

    handle existing user on signup

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,15 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { hashPassword } from '../utils/encryption';
-
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async user(
-    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput
   ): Promise<User | null> {
     return this.prisma.user.findUnique({
       where: userWhereUniqueInput,
@@ -17,6 +16,12 @@ export class UsersService {
   }
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const isExistingUser = await this.user({ email: data.email });
+
+    if (isExistingUser) {
+      throw new HttpException('User already exists', 400)
+    }
+
     const hashedPassword = await hashPassword(data.password);
 
     return this.prisma.user.create({
