ee95f2605b87919708ea16a6cbe4ce9b8e0dc6b4
Faiz Alkautsar <alkafaiz99@gmail.com>
2023-03-24 11:56:41 +0800

    update jwt expiry duration

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, { expiresIn: '15m' }),
     };
   }
 
